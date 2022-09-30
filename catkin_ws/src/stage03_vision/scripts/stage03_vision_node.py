a1e6f7f3bea43bd133958497c5219d18077a4e36
Mecasistem <mecasistem22@gmail.com>
2022-09-30 04:39:20 -0500

    Se logro la identificacion de objetos mediante segmentacion

--- catkin_ws/src/stage03_vision/scripts/stage03_vision_node.py
+++ catkin_ws/src/stage03_vision/scripts/stage03_vision_node.py
@@ -5,7 +5,6 @@ import time
 import numpy as np
 #%matplotlib inline
 import matplotlib.pyplot as plt
-import numpy as np
 import ros_numpy
 import time
 import tf
@@ -13,49 +12,63 @@ import moveit_commander
 import moveit_msgs.msg
 from gazebo_ros import gazebo_interface
 from sensor_msgs.msg import LaserScan, PointCloud2
-from geometry_msgs.msg import Pose, Quaternion ,TransformStamped
-from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Pose, Quaternion ,TransformStamped, Twist
 import sys
-from utils_notebooks import *
+from utils import *
 import cv2
 import os
-from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge
 
 #base_vel_pub = rospy.Publisher('/hsrb/command_velocity', Twist, queue_size=10)## Declaramos un publisher que ha 
-###de enviar mensajes tipo Twist al topico hsrb/command_velocity
+##de enviar mensajes tipo Twist al topico hsrb/command_velocity
 
-rospy.init_node("recognition")
 
-#def move_base_vel(vx, vy, vw):
-#    twist = Twist()
-#    twist.linear.x = vx
-#    twist.linear.y = vy
-#    twist.angular.z = vw 
-#    base_vel_pub.publish(twist)
+def callback_pcv(msg):
+    broadcaster= tf.TransformBroadcaster()
+    print("Point cloud received: " + str(msg.width) + "x" + str(msg.height))
+    arr = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
+    rgb = arr['rgb'].copy().view(np.uint32)
+    r,g,b = ((rgb >> 16) & 255), ((rgb >> 8) & 255), (rgb & 255)
+    img = cv2.merge((np.asarray(b,dtype='uint8'),np.asarray(g,dtype='uint8'),np.asarray(r,dtype='uint8')))
+    im_hsv = cv2.cvtColor(np.float32(img), cv2.COLOR_BGR2HSV)
+    h_min=0
+    h_max=145
+    region = (im_hsv > h_min) & (im_hsv < h_max)
+    idx,idy=np.where(region[:,:,0] )
+    mask= np.zeros((480,640))
+    mask[idx,idy]=255
+    kernel = np.ones((5, 5), np.uint8)
+    eroded_mask=cv2.erode(mask,kernel)
+    dilated_mask=cv2.dilate(eroded_mask,kernel)
+    #eroded_mask=cv2.erode(eroded_mask,kernel)
+    #dilated_mask=cv2.dilate(eroded_mask,kernel)
+    contours, hierarchy = cv2.findContours(dilated_mask.astype('uint8'),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    coord1 = []
+    coord2 = centroids(contours, arr, coord1)
+    #print("COORDENADAS: " + str(coord2))
+    aux = 0
+    for c in coord2:
+        x,y,z=c
+        print(c)
+        if np.isnan(x) or np.isnan(y) or np.isnan(z):
+            print('nan')
+        else:
+            broadcaster.sendTransform((x,y,z),(0,0,0,1), rospy.Time.now(), 'Object'+str(aux),"head_rgbd_sensor_link")
+            aux += 1
+
+
+def main():
     
-#move_base_vel(0 , 0, 3*np.pi)
+    #move_base(0,0,0.15*np.pi)
+    #move_h_g()
+    img = rospy.Subscriber('/hsrb/head_rgbd_sensor/depth_registered/rectified_points', PointCloud2, callback_pcv)
+    #rospy.Subscriber('hsrb/head_rgbd_sensor/rgb/image_rect_color', Image, callback_image)
+    rospy.spin()
 
-#listener = tf.TransformListener()
-#broadcaster= tf.TransformBroadcaster()
+if __name__ == '__main__':
+    main()
 
-#head = moveit_commander.MoveGroupCommander('head')
-#whole_body=moveit_commander.MoveGroupCommander('whole_body_weighted')
-#arm =  moveit_commander.MoveGroupCommander('arm')
-
-#arm.set_named_target('go')
-#arm.go()
-
-#head.go(np.array((0,-.15*np.pi)))
-
-
-rgbd = RGBD()
-
-image=rgbd.get_image()  #dimensiones de la imagen
-image.shape()    # una matriz (arreglo tipo numpy) 480px por 680 px 3 canales
-points= rgbd.get_points() 
-
-image.dtype()
-plt.imshow(image)
 
 
 
---
+++ catkin_ws/src/stage03_vision/scripts/utils_vision2.py
@@ -0,0 +1,60 @@
+import rospy
+import time
+import numpy as np
+import tf
+import moveit_commander
+import moveit_msgs.msg
+import cv2
+from geometry_msgs.msg import Twist
+
+rospy.init_node('base_and_sensor')    ### Conectamos/creamos un nodo llamado base and sensor 
+base_vel_pub = rospy.Publisher('/hsrb/command_velocity', Twist, queue_size=10)## Declaramos un publisher que ha 
+###de enviar mensajes tipo Twist al topico hsrb/command_velocity
+
+def move_base_vel(vx, vy, vw):
+    twist = Twist()
+    twist.linear.x = vx
+    twist.linear.y = vy
+    twist.angular.z = vw 
+    base_vel_pub.publish(twist)
+
+def move_base(x,y,yaw,timeout=5):
+    start_time = rospy.Time.now().to_sec()
+    while rospy.Time.now().to_sec() - start_time < timeout:  
+        move_base_vel(x, y, yaw)
+
+def move_h_g():
+    listener = tf.TransformListener()
+
+    head = moveit_commander.MoveGroupCommander('head')
+    whole_body=moveit_commander.MoveGroupCommander('whole_body_weighted')
+    arm =  moveit_commander.MoveGroupCommander('arm')
+
+    arm.set_named_target('go')
+    arm.go()
+
+    head.go(np.array((0,-.15*np.pi)))
+
+def centroids(contours, points, coord):
+    for contour in contours:
+        xyz=[]
+        M = cv2.moments(contour) #MOMENTOS ESADISTICOS DE LA IMAGEN
+    
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        boundRect = cv2.boundingRect(contour)
+
+        for jy in range (boundRect[0], boundRect[0]+boundRect[2]):
+            for ix in range(boundRect[1], boundRect[1]+boundRect[3]):
+                aux=(np.asarray((points['x'][ix,jy],points['y'][ix,jy],points['z'][ix,jy])))
+                if np.isnan(aux[0]) or np.isnan(aux[1]) or np.isnan(aux[2]):
+                    'reject point'
+                else:
+                    xyz.append(aux)
+                
+        xyz=np.asarray(xyz)
+        cent=xyz.mean(axis=0)
+        #print(cent)
+        #aux = np.asarray(cent)
+        coord.append(cent)
+    return coord
